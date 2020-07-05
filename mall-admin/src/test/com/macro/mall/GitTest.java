11cafba659f857ccc6808d5f7b41c4ea4978b1e9
yu_5dx <yu_5dx@qq.com>
2020-07-05 08:36:50 +0800

    第一次commit

---
+++ mall-admin/src/test/com/macro/mall/GitTest.java
@@ -0,0 +1,14 @@
+package macro.mall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class GitTest {
+    @Test
+    public void Diff(){
+
+    }
+}
